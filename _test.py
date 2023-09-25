853bb9423e70c6c48a2c3e3c2e6521779d92a512
iamsaicharan <charanvadakapur@gmail.com>
2023-09-25 02:18:09 -0400

    added the method to find the alpha and beta value of the stock

--- _test.py
+++ _test.py
@@ -2,12 +2,10 @@
 Script for testing the funcationality of the application
 ...
 
-Requirement
------------
+Requirement:
 pytest module -> pip install pytest
 
-To test
--------
+To test:
 > pytest
 
 """
@@ -17,16 +15,47 @@ import yfinance as yf
 
 def test_ticker_data_type():
     """
-    Asserts the data variable of Ticker Object - should assert data to Dataframe
+    Summary:
+    Asserts the data variable of Ticker Object
+
+    Return:
+    assert : boolean
+        should assert data to Dataframe
     """
     assert type(Ticker("AAPL").data).__name__ == "DataFrame"
 
 def test_ticker_fundamentals_type():
     """
-    Asserts the pe variable of Ticker Object - should assert pe to float
+    Summary:
+    Asserts the pe variable of Ticker Object
+
+    Return:
+    assert : boolean
+        should assert pe to dict
     """
     assert type(Ticker("AAPL").fundamentals) == dict
 
+def test_ticker_get_beta_method_return_type():
+    """
+    Summary:
+    Asserts the get_beta return of the TickerObject
+
+    Return:
+    assert : boolean
+        should assert get_beta return to float
+    """
+    assert type(Ticker("AAPL").get_beta()) == float
+
+def test_ticker_get_alpha_method_return_type():
+    """
+    Summary:
+    Asserts the get_alpha return of the TickerObject
+
+    Return:
+    assert : boolean
+        should assert get_alpha return to float
+    """
+    assert type(Ticker("AAPL").get_alpha("^GSPC")) == float
 
 
 
--- ticker.py
+++ ticker.py
@@ -5,10 +5,11 @@ class Ticker(object):
     """
     A class to represent a stock
     ...
-    Attributes
-    ----------
+    Attributes:
     stock : str
         stock ticker name
+    period : str
+        period selected for the data default: "10Y"
     data : Dataframe
         timeseries daily data of the stock
     fundamentals : dict
@@ -16,12 +17,56 @@ class Ticker(object):
     """
     def __init__(self, ticker, period = "10Y"):
         self.stock = ticker
+        self.period = period
         data = yf.download(ticker, period=period)
+        data.reset_index(inplace=True)
         data['daily_return'] = (data['Adj Close'] - data['Adj Close'].shift(1)) / data['Adj Close'].shift(1)
         data['cum_return'] = (1+data['daily_return']).cumprod()-1
         self.data = data
         self.fundamentals = yf.Ticker(ticker).info
 
+    def get_beta(self):
+        """
+        Summary:
+        A method to calculate the beta value of the stock
+
+        Return:
+        beta : float
+            return value which represents the beta of the stock
+        """
+        return self.fundamentals['beta']
+
+    def get_alpha(self, index, risk_free_rate=0.05):
+        """
+        Summary:
+        A method to calculate the alpha value of the stock which is a measure
+        to find how a stock is beating a benchmark
+
+        Parameters:
+        index : str
+            a string for the bench mark index eg: for snp500 -> ^GSPC
+        risk_free_rate : float
+            value of the risk free return value default: 0.05 i.e. 5%
+
+        Return:
+        alpha : float
+            return value which represents the alpha of the stock
+        """
+        index_data = yf.download(index, period=self.period)
+        index_data['daily_return'] = (index_data['Adj Close'] - index_data['Adj Close'].shift(1)) / index_data['Adj Close'].shift(1)
+        index_data['cum_return'] = (1+index_data['daily_return']).cumprod()-1
+        index_start_value = index_data['Adj Close'].iloc[0]
+        index_end_value = index_data['Adj Close'].iloc[-1]
+        stock_start_value = self.data['Adj Close'].iloc[0]
+        stock_end_value = self.data['Adj Close'].iloc[-1]
+        # Need to change to Actual return instead of simple return 
+        simple_return_index = (index_end_value - index_start_value)/index_start_value
+        simple_return_stock = (stock_end_value - stock_start_value)/stock_start_value
+        alpha = simple_return_stock - (risk_free_rate + self.get_beta()*(simple_return_index - risk_free_rate))
+        return float(alpha)
+
     def __str__(self):
-        return str(self.stock)
+        start_date = str(self.data['Date'].iloc[0])[:10]
+        end_date = str(self.data['Date'].iloc[-1])[:10]
+        return str(self.stock).upper() + " [" + start_date + " - " + end_date + "]"
     
