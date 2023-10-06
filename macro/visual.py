3ba3f79acc577078b46a608adf9d0f4e6ee8bcbe
iamsaicharan <charanvadakapur@gmail.com>
2023-10-05 20:42:23 -0400

    added the doc string for the macro.visuals

--- macro/visual.py
+++ macro/visual.py
@@ -4,8 +4,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Visual(Analysis):
+    """
+    A class for visualization of macro objects extends Analysis class from macro.analysis
+    ...
+    Attributes:
+    macros (from parent class) : list
+        list of macros objects
 
+    Methods:
+    visualize_GDP(self, period=constants.PERIOD)
+        get the visualization for GDP data for the object macros
+    """
     def visualize_GDP(self, period=constants.PERIOD):
+        """
+        Summary:
+        A method to get visualization for gdp of the object macros
+
+        Parameters:
+        period : str
+            a string for period: "10Y"
+
+        Return:
+        plt : module
+            returns the object displays the matplotlib plot of the graph
+        """
         df = self.get_merged_GDP(period=period)
         df.set_index('Date', inplace=True)
         for column in df.columns:
