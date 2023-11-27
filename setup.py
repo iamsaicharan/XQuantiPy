180edb6c746aa189cea368a285453908858591d9
iamsaicharan <charanvadakapur@gmail.com>
2023-11-27 00:51:38 -0500

    added new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 # print(setuptools.find_packages())
 setuptools.setup(
     name='xquantipy',
-    version='0.2.1',
+    version='0.2.2',
     author='Sai Charan Vadakapur',
     description='Module for financial analysis',
     packages=setuptools.find_packages(),
