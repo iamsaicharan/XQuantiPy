21ccd683b18a02bf49a89bd9f758929bb18a8853
iamsaicharan <charanvadakapur@gmail.com>
2023-12-02 01:37:02 -0500

    asset error fix

--- xquantipy/fetcher/scraper.py
+++ xquantipy/fetcher/scraper.py
@@ -33,7 +33,7 @@ class Fetcher:
         """
         assert type(indicators) == list, "Error: Invalid indicators type"
         assert type(country_code) == str, "Error: Invalid country code type"
-        assert type(dates) == set, "Error: Invalid dates type"
+        assert type(dates) == tuple, "Error: Invalid dates type"
         dfs = []
         for i in indicators:
             base_url = "http://www.macrotrends.net/countries/"
